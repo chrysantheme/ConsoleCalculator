b20008f61893d36a56b3d035f5b805be16b25799
Sandra Seehars <s.seehars@gmx.ch>
2019-12-18 14:56:31 +0100

    Implement addition method in Calculator class

---
+++ src/main/java/ch/bbw/ss/ConsoleCalculator/Calculator.java
@@ -0,0 +1,16 @@
+package ch.bbw.ss.ConsoleCalculator;
+
+/**
+ * Calculator Klasse
+ * 	- kann zwei Zahlen addieren
+ * 
+ * @author Sandra Seehars
+ * @version 18.12.2019
+ */
+
+public class Calculator {
+
+	public int summe(int summand1, int summand2) {
+		return summand1 + summand2; 
+	}
+}
