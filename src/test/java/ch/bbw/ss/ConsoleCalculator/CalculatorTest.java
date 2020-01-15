5350f15f6c744a0b1a017a9c3cda9d800ea4fabf
Sandra Seehars <s.seehars@gmx.ch>
2020-01-15 10:53:01 +0100

    Implement second test for subtraction where two numbers are negative

--- src/test/java/ch/bbw/ss/ConsoleCalculator/CalculatorTest.java
+++ src/test/java/ch/bbw/ss/ConsoleCalculator/CalculatorTest.java
@@ -26,4 +26,16 @@ public class CalculatorTest {
 		testee = new Calculator();
 		assertTrue(testee.subtraktion(25,  10) == 15);
 	}
+	
+	@Test
+	public void testSummeZweiNegativeIsOk() {
+		testee = new Calculator();
+		assertTrue(testee.summe(-10,  -25) == -35);
+	}
+	
+	@Test
+	public void testSubtraktionZweiNegativeIsOk() {
+		testee = new Calculator();
+		assertTrue(testee.subtraktion(-25,  -10) == -15);
+	}
 }
